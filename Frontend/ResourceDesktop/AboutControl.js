3058f84c5a6e010c79158671c6e3bbc5abf49270
Peakk2011 <peakk3984@gmail.com>
2024-11-16 20:30:20 +0700

    v.1.1.30

--- Frontend/ResourceDesktop/AboutControl.js
+++ Frontend/ResourceDesktop/AboutControl.js
@@ -3,7 +3,7 @@ const Contact = document.getElementById('Email');
 function rippleSuggess() {
 
   Contact.classList.add('expand');
-  
+
 }
 
 function CloseRipple() {
@@ -12,3 +12,79 @@ function CloseRipple() {
   event.stopPropagation();
 
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const ScrollEventToAbout = document.getElementById('ScrollEventToAbout');
+  const scrollContainer = document.querySelector('main');
+  const sectionsDetails = document.querySelectorAll('.ScrollAbout2');
+  let currentIndex = 0;
+
+  ScrollEventToAbout.addEventListener('click', function () {
+    if (sectionsDetails.length > 1) {
+      currentIndex = (currentIndex + 1) % sectionsDetails.length;
+    } else {
+      currentIndex = (currentIndex + 1) % 2;  // เลื่อนกลับไปที่จุดเริ่มต้นถ้าเป็น section เดียว
+    }
+    sectionsDetails[currentIndex % sectionsDetails.length].scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+// Ripple theme effect
+
+document.addEventListener('DOMContentLoaded', function () {
+  const themeSwitcher = document.getElementById('theme-switcher');
+
+  // ตั้งค่าเริ่มต้นสำหรับธีมจาก localStorage
+  const currentTheme = localStorage.getItem('theme') || 'dark-mode';
+  document.body.classList.add(currentTheme);
+  themeSwitcher.addEventListener('click', function (event) {
+
+    if (document.body.classList.contains('dark-mode')) {
+      document.body.classList.remove('dark-mode');
+      document.body.classList.add('light-mode');
+      localStorage.setItem('theme', 'light-mode');
+    }
+    else {
+      document.body.classList.remove('light-mode');
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark-mode');
+    }
+
+  });
+});
+
+const DotsBeforeActiveE = document.getElementById("DotsBeforeActiveE");
+const DotsAFActiveE = document.getElementById('DotsAFActiveE')
+
+DotsBeforeActiveE.addEventListener("click", () => {
+  DotsBeforeActiveE.classList.add('DotsActiveOLD');
+  DotsAFActiveE.classList.remove('DotsActive');
+})
+
+DotsAFActiveE.addEventListener('click', () => {
+  DotsAFActiveE.classList.add("DotsActive");
+  DotsBeforeActiveE.classList.remove('DotsActiveOLD');
+})
+
+const sections = document.querySelectorAll('.mainSection, .ScrollAbout2');
+// สร้างตัว observer
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains('mainSection')) {
+        DotsAFActiveE.classList.add("DotsActive");
+        DotsBeforeActiveE.classList.remove('DotsActiveOLD');
+      } else if (entry.target.classList.contains('ScrollAbout2')) {
+        DotsBeforeActiveE.classList.add('DotsActiveOLD');
+        DotsAFActiveE.classList.remove('DotsActive');
+      }
+    }
+  });
+}, {
+  threshold: 0.5 // 50% section scroll viewport
+});
+
+// สังเกตทุก section
+sections.forEach(section => {
+  observer.observe(section);
+});
